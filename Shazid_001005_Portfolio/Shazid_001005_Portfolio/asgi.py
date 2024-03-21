ff16414b996c3ac870c283680d9ec165d324f2fd
Shazid Hasan <amj.shazidhasan@gmail.com>
2024-03-21 14:55:57 -0500

    sub commit
    Almost complete

--- Shazid_001005_Portfolio/Shazid_001005_Portfolio/asgi.py
+++ Shazid_001005_Portfolio/Shazid_001005_Portfolio/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for name_id_portfolio project.
+ASGI config for Shazid_001005_Portfolio project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'name_id_portfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shazid_001005_Portfolio.settings')
 
 application = get_asgi_application()
--- Shazid_001005_Portfolio/manage.py
+++ Shazid_001005_Portfolio/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'name_id_portfolio.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shazid_001005_Portfolio.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
